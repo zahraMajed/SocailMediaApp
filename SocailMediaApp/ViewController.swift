ac7f4f6d594b52c7eeccb3a0bd03b4e8903314dd
zahraMajed <zahra_m.alzawad@hotmail.com>
2022-05-09 14:15:00 +0300

    Initial Commit

---
+++ SocailMediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocailMediaApp
+//
+//  Created by Zahra Majed on 08/10/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
